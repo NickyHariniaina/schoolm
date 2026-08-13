5f8757224da7d395b36bfb32f6a2e17133802670
NickyHariniaina <ferrissushi@gmail.com>
2026-08-13 15:07:57 +0300

    chore: add JAdmin

---
+++ src/main/java/hei/student/schoolm/repository/model/JAdmin.java
@@ -0,0 +1,19 @@
+package hei.student.schoolm.repository.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+@Entity
+@Table(name = "admin")
+public class JAdmin extends JUser {
+  @Id private String id;
+}
