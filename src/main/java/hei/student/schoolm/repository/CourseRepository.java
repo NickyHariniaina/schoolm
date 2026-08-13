ef9e6fa78af94811808726f5c0ded55dd8771fef
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-08-13 19:04:10 +0300

    feat: add Course and Teacher repositories

---
+++ src/main/java/hei/student/schoolm/repository/CourseRepository.java
@@ -0,0 +1,7 @@
+package hei.student.schoolm.repository;
+
+import hei.student.schoolm.repository.model.JCourse;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseRepository extends JpaRepository<JCourse, UUID> {}
