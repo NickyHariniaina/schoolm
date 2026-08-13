a1b6b359292f39fb0d741aa442453dc1df8cfe25
NickyHariniaina <ferrissushi@gmail.com>
2026-08-13 16:18:18 +0300

    style: format code

--- src/main/java/hei/student/schoolm/repository/model/JCohort.java
+++ src/main/java/hei/student/schoolm/repository/model/JCohort.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import java.time.Instant;
 import java.util.ArrayList;
