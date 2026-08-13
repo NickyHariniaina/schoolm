aec77b381acaca17d50f9c725b0856f5b750f970
NickyHariniaina <ferrissushi@gmail.com>
2026-08-13 20:44:58 +0300

    fix: return 400 on unreadable request body

--- src/main/java/hei/student/schoolm/exception/GlobalExceptionHandler.java
+++ src/main/java/hei/student/schoolm/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import hei.student.schoolm.exception.model.ApiException;
 import jakarta.servlet.http.HttpServletRequest;
 import java.time.Instant;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -26,10 +27,11 @@ public class GlobalExceptionHandler {
 
   @ExceptionHandler({
     MethodArgumentNotValidException.class,
-    MethodArgumentTypeMismatchException.class
+    MethodArgumentTypeMismatchException.class,
+    HttpMessageNotReadableException.class
   })
-  public ResponseEntity<ExceptionBody> handleMethodArgumentNotValidException(
-      MethodArgumentNotValidException exception, HttpServletRequest request) {
+  public ResponseEntity<ExceptionBody> handleBadRequest(
+      Exception exception, HttpServletRequest request) {
     return ResponseEntity.badRequest()
         .body(
             new ExceptionBody(
