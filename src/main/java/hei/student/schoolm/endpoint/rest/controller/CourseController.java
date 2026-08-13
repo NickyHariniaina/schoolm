8f5c675f3866c8510368b4db40ca25f1d30d23be
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-08-13 23:46:02 +0300

    feat: add GET endpoints (/courses & /courses/{courseId}) for courses

--- src/main/java/hei/student/schoolm/endpoint/rest/controller/CourseController.java
+++ src/main/java/hei/student/schoolm/endpoint/rest/controller/CourseController.java
@@ -5,6 +5,7 @@ import hei.student.schoolm.dto.TeacherIdsRequest;
 import hei.student.schoolm.mapper.CourseMapper;
 import hei.student.schoolm.service.CourseService;
 import jakarta.validation.Valid;
+import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,18 @@ import org.springframework.web.bind.annotation.*;
 public class CourseController {
   private final CourseService courseService;
   private final CourseMapper courseMapper;
+
+  @GetMapping
+  public List<CourseDto> getAllCourses() {
+    var courses = courseService.getAllCourses();
+    return courseMapper.toDtoList(courses);
+  }
+
+  @GetMapping("/{courseId}")
+  public CourseDto getCourseById(@PathVariable UUID courseId) {
+    var course = courseService.getCourseById(courseId);
+    return courseMapper.toDto(course);
+  }
 
   @PutMapping("/{courseId}/teacher")
   public CourseDto assignTeachers(
