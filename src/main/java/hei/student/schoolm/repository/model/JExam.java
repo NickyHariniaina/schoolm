831047306bf024d36faf160263925f1170bd1733
NickyHariniaina <ferrissushi@gmail.com>
2026-08-13 15:08:19 +0300

    chore: add JExam

---
+++ src/main/java/hei/student/schoolm/repository/model/JExam.java
@@ -0,0 +1,53 @@
+package hei.student.schoolm.repository.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+@Entity
+@Table(name = "exam")
+public class JExam {
+  @Id private String id;
+
+  @ManyToOne
+  @JoinColumn(name = "course_id", nullable = false)
+  private JCourse course;
+
+  @Column(nullable = false)
+  private LocalDate dateExam;
+
+  @Column(nullable = false)
+  private int coefNumerator;
+
+  @Column(nullable = false)
+  private int coefDenominator;
+
+  @OneToMany(mappedBy = "exam")
+  private List<JGrade> grades = new ArrayList<>();
+
+  @CreationTimestamp
+  @Column(updatable = false)
+  private Instant createdAt;
+
+  @UpdateTimestamp
+  @Column
+  private Instant updatedAt;
+}
