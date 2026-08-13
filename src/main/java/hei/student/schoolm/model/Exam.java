a9ef9e2d77d9e005303360e1bc61e122293534ec
NickyHariniaina <ferrissushi@gmail.com>
2026-08-13 19:36:10 +0300

    fix: exam domain model with grades

--- src/main/java/hei/student/schoolm/model/Exam.java
+++ src/main/java/hei/student/schoolm/model/Exam.java
@@ -3,6 +3,7 @@ package hei.student.schoolm.model;
 import hei.student.schoolm.util.Fraction;
 import java.time.Instant;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,6 +20,7 @@ public class Exam {
   private UUID id;
   private Course course;
   private LocalDate dateExam;
+  private List<Grade> grades;
   private Fraction coefficient;
   private Instant createdAt;
   private Instant updatedAt;
