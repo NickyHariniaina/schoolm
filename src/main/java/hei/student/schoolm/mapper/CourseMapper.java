363118c0d79e927e3ba1f06c1aa2b68ccdbbffee
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-08-13 19:48:49 +0300

    feat: add CourseMapper

---
+++ src/main/java/hei/student/schoolm/mapper/CourseMapper.java
@@ -0,0 +1,27 @@
+package hei.student.schoolm.mapper;
+
+import hei.student.schoolm.dto.CourseDto;
+import hei.student.schoolm.repository.model.JCourse;
+import hei.student.schoolm.repository.model.JTeacher;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourseMapper {
+    public CourseDto toDto(JCourse course) {
+        CourseDto dto = new CourseDto();
+        dto.setId(course.getId());
+        dto.setRef(course.getRef());
+        dto.setTitle(course.getTitle());
+        dto.setCredit(course.getCredit());
+        dto.setTrack(course.getTrack().name());
+        dto.setSemester(course.getSemester().name());
+        dto.setTeacherIds(mapTeacherIds(course));
+        return dto;
+    }
+
+  private List<UUID> mapTeacherIds(JCourse course) {
+    return course.getTeachers().stream().map(JTeacher::getId).toList();
+  }
+}
