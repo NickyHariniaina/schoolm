4e56afc04aadf382eaeb621e770defdb4bbb0d69
NickyHariniaina <ferrissushi@gmail.com>
2026-08-13 16:39:47 +0300

    fix: move track to group in jpa entities

--- src/main/java/hei/student/schoolm/repository/model/JGroup.java
+++ src/main/java/hei/student/schoolm/repository/model/JGroup.java
@@ -1,13 +1,18 @@
 package hei.student.schoolm.repository.model;
 
+import hei.student.schoolm.model.Track;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,9 +32,16 @@ import org.hibernate.annotations.UpdateTimestamp;
 public class JGroup {
   @Id private String id;
 
+  @NotBlank
   @Column(nullable = false)
   private String ref;
 
+  @NotNull
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private Track track;
+
+  @NotNull
   @ManyToOne
   @JoinColumn(name = "cohort_id", nullable = false)
   private JCohort cohort;
