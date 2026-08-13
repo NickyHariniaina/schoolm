4caffde0d48c33f7641aa6fb96f4a37719bc6344
NickyHariniaina <ferrissushi@gmail.com>
2026-08-13 14:16:47 +0300

    chore: add exam domain model

---
+++ src/main/java/hei/student/schoolm/model/Exam.java
@@ -0,0 +1,25 @@
+package hei.student.schoolm.model;
+
+import hei.student.schoolm.util.Fraction;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Exam {
+  private UUID id;
+  private Course course;
+  private LocalDate dateExam;
+  private Fraction coefficient;
+  private Instant createdAt;
+  private Instant updatedAt;
+}
