80a0626d9ff45352022cfc70904489e6a6e2712a
NickyHariniaina <ferrissushi@gmail.com>
2026-08-13 16:14:25 +0300

    chore: add validation JCourse

--- src/main/java/hei/student/schoolm/repository/model/JCourse.java
+++ src/main/java/hei/student/schoolm/repository/model/JCourse.java
@@ -12,6 +12,9 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
@@ -31,19 +34,24 @@ import org.hibernate.annotations.UpdateTimestamp;
 public class JCourse {
   @Id private String id;
 
+  @NotBlank
   @Column(nullable = false, unique = true)
   private String ref;
 
+  @NotBlank
   @Column(nullable = false)
   private String title;
 
+  @Positive
   @Column(nullable = false)
   private int credit;
 
+  @NotNull
   @Enumerated(EnumType.STRING)
   @Column(nullable = false)
   private Track track;
 
+  @NotNull
   @Enumerated(EnumType.STRING)
   @Column(nullable = false)
   private Semester semester;
