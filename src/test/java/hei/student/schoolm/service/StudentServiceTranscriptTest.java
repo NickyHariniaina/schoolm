238a4aa2fbea181de166b51c72ff435d3fb6232f
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-08-17 00:17:09 +0300

    style: format code

--- src/test/java/hei/student/schoolm/service/StudentServiceTranscriptTest.java
+++ src/test/java/hei/student/schoolm/service/StudentServiceTranscriptTest.java
@@ -104,7 +104,8 @@ class StudentServiceTranscriptTest {
                 createCourse(COURSE_S3_ID, "S3C", Semester.S3, List.of(), List.of(), Track.COMMON),
                 createCourse(COURSE_S4_ID, "S4C", Semester.S4, List.of(), List.of(), Track.COMMON),
                 createCourse(COURSE_S5_ID, "S5C", Semester.S5, List.of(), List.of(), Track.COMMON),
-                createCourse(COURSE_S6_ID, "S6C", Semester.S6, List.of(), List.of(), Track.COMMON)));
+                createCourse(
+                    COURSE_S6_ID, "S6C", Semester.S6, List.of(), List.of(), Track.COMMON)));
     var student = createStudent(group);
 
     var today = LocalDate.now();
@@ -153,10 +154,10 @@ class StudentServiceTranscriptTest {
         List.of(
             createCourse(COURSE_S4_ID, "LV2", Semester.S4, List.of(), List.of(), Track.COMMON),
             createCourse(COURSE_S3_ID, "PROG4", Semester.S3, List.of(), List.of(), Track.EL),
-            createCourse(COURSE_S1_ID, "THEORIE1", Semester.S1, List.of(), List.of(), Track.COMMON));
+            createCourse(
+                COURSE_S1_ID, "THEORIE1", Semester.S1, List.of(), List.of(), Track.COMMON));
 
-    var result =
-        studentService.filterCourses(courses, List.of(Semester.S3, Semester.S4), Track.EL);
+    var result = studentService.filterCourses(courses, List.of(Semester.S3, Semester.S4), Track.EL);
 
     assertEquals(List.of("PROG4", "LV2"), result.stream().map(Course::getRef).toList());
   }
@@ -171,7 +172,12 @@ class StudentServiceTranscriptTest {
     var grade2 = createGrade(GRADE_2_ID, student, exam2, new BigDecimal("12"));
     var fullCourse =
         createCourse(
-            COURSE_S3_ID, "PROG4", Semester.S3, List.of(exam1, exam2), List.of(grade1, grade2), Track.EL);
+            COURSE_S3_ID,
+            "PROG4",
+            Semester.S3,
+            List.of(exam1, exam2),
+            List.of(grade1, grade2),
+            Track.EL);
 
     var result = studentService.computeFinalGrade(fullCourse, student);
 
@@ -187,7 +193,8 @@ class StudentServiceTranscriptTest {
     var student = createStudent(null);
     var grade1 = createGrade(GRADE_1_ID, student, exam1, new BigDecimal("14"));
     var fullCourse =
-        createCourse(COURSE_S3_ID, "PROG4", Semester.S3, List.of(exam1, exam2), List.of(grade1), Track.EL);
+        createCourse(
+            COURSE_S3_ID, "PROG4", Semester.S3, List.of(exam1, exam2), List.of(grade1), Track.EL);
 
     var result = studentService.computeFinalGrade(fullCourse, student);
 
@@ -223,7 +230,8 @@ class StudentServiceTranscriptTest {
     var exam1 = createExam(EXAM_1_ID, course, new Fraction(1, 2));
     var exam2 = createExam(EXAM_2_ID, course, new Fraction(1, 2));
     var fullCourse =
-        createCourse(COURSE_S3_ID, "PROG4", Semester.S3, List.of(exam1, exam2), List.of(), Track.EL);
+        createCourse(
+            COURSE_S3_ID, "PROG4", Semester.S3, List.of(exam1, exam2), List.of(), Track.EL);
 
     var expectedStatus = TranscriptStatus.COMPLET;
     assertEquals(expectedStatus, studentService.computeStatus(fullCourse, Semester.S3));
@@ -235,7 +243,8 @@ class StudentServiceTranscriptTest {
     var exam1 = createExam(EXAM_1_ID, course, new Fraction(1, 3));
     var exam2 = createExam(EXAM_2_ID, course, new Fraction(1, 3));
     var fullCourse =
-        createCourse(COURSE_S3_ID, "PROG4", Semester.S3, List.of(exam1, exam2), List.of(), Track.EL);
+        createCourse(
+            COURSE_S3_ID, "PROG4", Semester.S3, List.of(exam1, exam2), List.of(), Track.EL);
 
     var expectedStatus = TranscriptStatus.INCOMPLET;
     assertEquals(expectedStatus, studentService.computeStatus(fullCourse, Semester.S3));
@@ -247,7 +256,8 @@ class StudentServiceTranscriptTest {
     var exam1 = createExam(EXAM_1_ID, course, new Fraction(1, 2));
     var exam2 = createExam(EXAM_2_ID, course, new Fraction(1, 2));
     var fullCourse =
-        createCourse(COURSE_S4_ID, "PROG4", Semester.S4, List.of(exam1, exam2), List.of(), Track.EL);
+        createCourse(
+            COURSE_S4_ID, "PROG4", Semester.S4, List.of(exam1, exam2), List.of(), Track.EL);
 
     var expectedStatus = TranscriptStatus.INCOMPLET;
     assertEquals(expectedStatus, studentService.computeStatus(fullCourse, Semester.S3));
@@ -266,9 +276,11 @@ class StudentServiceTranscriptTest {
   @Test
   void should_return_COMPLET_global_when_all_courses_COMPLET() {
     var cohort = createCohort(2024);
-    var s1Course = createCourse(COURSE_S1_ID, "MATH1", Semester.S1, List.of(), List.of(), Track.COMMON);
+    var s1Course =
+        createCourse(COURSE_S1_ID, "MATH1", Semester.S1, List.of(), List.of(), Track.COMMON);
     var s1Exam = createExam(EXAM_1_ID, s1Course, new Fraction(1, 1));
-    var s2Course = createCourse(COURSE_S2_ID, "PROG2", Semester.S2, List.of(), List.of(), Track.COMMON);
+    var s2Course =
+        createCourse(COURSE_S2_ID, "PROG2", Semester.S2, List.of(), List.of(), Track.COMMON);
     var s2Exam = createExam(EXAM_2_ID, s2Course, new Fraction(1, 1));
     var student = createStudent(null);
     var s1Full =
@@ -278,7 +290,7 @@ class StudentServiceTranscriptTest {
             Semester.S1,
             List.of(s1Exam),
             List.of(createGrade(GRADE_1_ID, student, s1Exam, new BigDecimal("15"))),
-                Track.COMMON);
+            Track.COMMON);
     var s2Full =
         createCourse(
             COURSE_S2_ID,
@@ -286,7 +298,7 @@ class StudentServiceTranscriptTest {
             Semester.S2,
             List.of(s2Exam),
             List.of(createGrade(GRADE_2_ID, student, s2Exam, new BigDecimal("16"))),
-                Track.COMMON);
+            Track.COMMON);
     var group = createGroup(cohort, List.of(s1Full, s2Full));
     var fullStudent = createStudent(group);
 
@@ -299,14 +311,17 @@ class StudentServiceTranscriptTest {
   @Test
   void should_return_INCOMPLET_global_when_any_course_INCOMPLET() {
     var cohort = createCohort(2024);
-    var s1Course = createCourse(COURSE_S1_ID, "MATH1", Semester.S1, List.of(), List.of(), Track.COMMON);
+    var s1Course =
+        createCourse(COURSE_S1_ID, "MATH1", Semester.S1, List.of(), List.of(), Track.COMMON);
     var s1Exam1 = createExam(EXAM_1_ID, s1Course, new Fraction(1, 3));
     var s1Exam2 = createExam(EXAM_2_ID, s1Course, new Fraction(1, 3));
-    var s2Course = createCourse(COURSE_S2_ID, "PROG2", Semester.S2, List.of(), List.of(), Track.COMMON);
+    var s2Course =
+        createCourse(COURSE_S2_ID, "PROG2", Semester.S2, List.of(), List.of(), Track.COMMON);
     var s2Exam = createExam(EXAM_2_ID, s2Course, new Fraction(1, 1));
     var student = createStudent(null);
     var s1Full =
-        createCourse(COURSE_S1_ID, "MATH1", Semester.S1, List.of(s1Exam1, s1Exam2), List.of(), Track.COMMON);
+        createCourse(
+            COURSE_S1_ID, "MATH1", Semester.S1, List.of(s1Exam1, s1Exam2), List.of(), Track.COMMON);
     var s2Full =
         createCourse(
             COURSE_S2_ID,
@@ -314,7 +329,7 @@ class StudentServiceTranscriptTest {
             Semester.S2,
             List.of(s2Exam),
             List.of(createGrade(GRADE_2_ID, student, s2Exam, new BigDecimal("16"))),
-                Track.COMMON);
+            Track.COMMON);
     var group = createGroup(cohort, List.of(s1Full, s2Full));
     var fullStudent = createStudent(group);
 
@@ -327,7 +342,8 @@ class StudentServiceTranscriptTest {
   void should_return_empty_courses_when_group_has_no_matching_courses() {
     var cohort = createCohort(2024);
     var s5Course = createCourse(COURSE_S5_ID, "ALGO5", Semester.S5, List.of(), List.of(), Track.EL);
-    var s6Course = createCourse(COURSE_S6_ID, "THESE6", Semester.S6, List.of(), List.of(), Track.COMMON);
+    var s6Course =
+        createCourse(COURSE_S6_ID, "THESE6", Semester.S6, List.of(), List.of(), Track.COMMON);
     var group = createGroup(cohort, List.of(s5Course, s6Course));
     var student = createStudent(group);
 
@@ -340,9 +356,11 @@ class StudentServiceTranscriptTest {
   @Test
   void should_build_transcript_dto_with_student_group_cohort_info() {
     var cohort = createCohort(2024);
-    var s1Course = createCourse(COURSE_S1_ID, "MATH1", Semester.S1, List.of(), List.of(), Track.COMMON);
+    var s1Course =
+        createCourse(COURSE_S1_ID, "MATH1", Semester.S1, List.of(), List.of(), Track.COMMON);
     var s1Exam = createExam(EXAM_1_ID, s1Course, new Fraction(1, 1));
-    var s2Course = createCourse(COURSE_S2_ID, "PROG2", Semester.S2, List.of(), List.of(), Track.COMMON);
+    var s2Course =
+        createCourse(COURSE_S2_ID, "PROG2", Semester.S2, List.of(), List.of(), Track.COMMON);
     var s2Exam = createExam(EXAM_2_ID, s2Course, new Fraction(1, 1));
     var student = createStudent(null);
     var s1Full =
@@ -352,7 +370,7 @@ class StudentServiceTranscriptTest {
             Semester.S1,
             List.of(s1Exam),
             List.of(createGrade(GRADE_1_ID, student, s1Exam, new BigDecimal("15"))),
-                Track.COMMON);
+            Track.COMMON);
     var s2Full =
         createCourse(
             COURSE_S2_ID,
@@ -360,7 +378,7 @@ class StudentServiceTranscriptTest {
             Semester.S2,
             List.of(s2Exam),
             List.of(createGrade(GRADE_2_ID, student, s2Exam, new BigDecimal("16"))),
-                Track.COMMON);
+            Track.COMMON);
     var group = createGroup(cohort, List.of(s1Full, s2Full));
     var fullStudent = createStudent(group);
 
@@ -381,7 +399,8 @@ class StudentServiceTranscriptTest {
   @Test
   void should_expose_null_value_for_exam_without_grade() {
     var cohort = createCohort(2024);
-    var s2Course = createCourse(COURSE_S2_ID, "PROG2", Semester.S2, List.of(), List.of(), Track.COMMON);
+    var s2Course =
+        createCourse(COURSE_S2_ID, "PROG2", Semester.S2, List.of(), List.of(), Track.COMMON);
     var exam1 = createExam(EXAM_1_ID, s2Course, new Fraction(1, 2));
     var exam2 = createExam(EXAM_2_ID, s2Course, new Fraction(1, 2));
     var student = createStudent(null);
@@ -391,7 +410,8 @@ class StudentServiceTranscriptTest {
             "PROG2",
             Semester.S2,
             List.of(exam1, exam2),
-            List.of(createGrade(GRADE_1_ID, student, exam1, new BigDecimal("14"))), Track.COMMON);
+            List.of(createGrade(GRADE_1_ID, student, exam1, new BigDecimal("14"))),
+            Track.COMMON);
     var group = createGroup(cohort, List.of(s2Full));
     var fullStudent = createStudent(group);
 
