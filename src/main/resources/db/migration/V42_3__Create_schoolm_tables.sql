d7e8a42d85308b1c68b59867370a4876d55af95b
NickyHariniaina <ferrissushi@gmail.com>
2026-08-16 23:25:52 +0300

    hotfix: fix migration constraint issue

--- src/main/resources/db/migration/V42_3__Create_schoolm_tables.sql
+++ src/main/resources/db/migration/V42_3__Create_schoolm_tables.sql
@@ -1,18 +1,18 @@
 create table cohort
 (
-    id         varchar primary key,
+    id         uuid primary key default gen_random_uuid(),
     ref        varchar not null,
-    entry_year integer  not null,
+    entry_year integer not null,
     created_at timestamptz,
     updated_at timestamptz
 );
 
 create table student_group
 (
-    id         varchar primary key,
+    id         uuid primary key default gen_random_uuid(),
     ref        varchar not null,
     track      varchar not null,
-    cohort_id  varchar   not null
+    cohort_id  uuid   not null
         constraint student_group_cohort_id_fk references cohort (id),
     created_at timestamptz,
     updated_at timestamptz
@@ -20,10 +20,10 @@ create table student_group
 
 create table student
 (
-    id         varchar primary key,
+    id         uuid primary key default gen_random_uuid(),
     reference  varchar not null
         constraint student_reference_unique unique,
-    group_id   varchar
+    group_id   uuid
         constraint student_group_id_fk references student_group (id),
     email      varchar not null
         constraint student_email_unique unique,
@@ -36,7 +36,7 @@ create table student
 
 create table teacher
 (
-    id         varchar primary key,
+    id         uuid primary key default gen_random_uuid(),
     email      varchar not null
         constraint teacher_email_unique unique,
     first_name varchar not null,
@@ -48,7 +48,7 @@ create table teacher
 
 create table admin
 (
-    id         varchar primary key,
+    id         uuid primary key default gen_random_uuid(),
     email      varchar not null
         constraint admin_email_unique unique,
     first_name varchar not null,
@@ -60,11 +60,11 @@ create table admin
 
 create table course
 (
-    id         varchar primary key,
+    id         uuid primary key default gen_random_uuid(),
     ref        varchar not null
         constraint course_ref_unique unique,
     title      varchar not null,
-    credit     integer  not null,
+    credit     integer not null,
     track      varchar not null,
     semester   varchar not null,
     created_at timestamptz,
@@ -73,42 +73,42 @@ create table course
 
 create table course_group
 (
-    course_id varchar not null
+    course_id uuid not null
         constraint course_group_course_id_fk references course (id),
-    group_id  varchar not null
+    group_id  uuid not null
         constraint course_group_group_id_fk references student_group (id),
     primary key (course_id, group_id)
 );
 
 create table course_teacher
 (
-    course_id  varchar not null
+    course_id  uuid not null
         constraint course_teacher_course_id_fk references course (id),
-    teacher_id varchar not null
+    teacher_id uuid not null
         constraint course_teacher_teacher_id_fk references teacher (id),
     primary key (course_id, teacher_id)
 );
 
 create table exam
 (
-    id               varchar primary key,
-    course_id        varchar   not null
+    id               uuid primary key default gen_random_uuid(),
+    course_id        uuid   not null
         constraint exam_course_id_fk references course (id),
-    date_exam        date      not null,
-    coef_numerator   integer   not null,
-    coef_denominator integer   not null,
+    date_exam        date   not null,
+    coef_numerator   integer not null,
+    coef_denominator integer not null,
     created_at       timestamptz,
     updated_at       timestamptz
 );
 
 create table grade
 (
-    id            varchar primary key,
-    student_id    varchar        not null
+    id            uuid primary key default gen_random_uuid(),
+    student_id    uuid        not null
         constraint grade_student_id_fk references student (id),
-    exam_id       varchar        not null
+    exam_id       uuid        not null
         constraint grade_exam_id_fk references exam (id),
-    value         numeric(4, 2)  not null,
+    value         numeric(4, 2) not null,
     change_reason varchar,
     created_at    timestamptz,
     updated_at    timestamptz
