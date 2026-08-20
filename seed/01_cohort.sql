7b471e13d0437134ef1a052c5880cd6b84a97837
NickyHariniaina <ferrissushi@gmail.com>
2026-08-20 17:08:38 +0300

    chore: remove seed sql files

--- seed/01_cohort.sql
+++
@@ -1,4 +0,0 @@
-insert into cohort (ref, entry_year) values
-  ('K', 2024),
-  ('J', 2023),
-  ('N', 2025);
--- seed/08_admin.sql
+++
@@ -1,2 +0,0 @@
-insert into admin (email, first_name, last_name, role, password) values
-  ('admin@hei.school', 'Admin', 'Principal', 'ADMIN', '$2a$10$vQ.M0zvBtLJuyrSadV0q8uarFVF8LIMCct7Jeq9xBhLPOzhyEDtae');
