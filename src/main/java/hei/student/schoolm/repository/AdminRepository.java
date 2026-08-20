eb07c798adb0cc1745bd5b98745626a048800b07
NickyHariniaina <ferrissushi@gmail.com>
2026-08-20 17:08:22 +0300

    refactor: shorten jAdminRepository and jAdminMapper in AdminRepository

--- src/main/java/hei/student/schoolm/repository/AdminRepository.java
+++ src/main/java/hei/student/schoolm/repository/AdminRepository.java
@@ -12,16 +12,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 @RequiredArgsConstructor
 public class AdminRepository {
-  private final JAdminRepository jAdminRepository;
-  private final JAdminMapper jAdminMapper;
+  private final JAdminRepository repository;
+  private final JAdminMapper mapper;
 
   @Transactional(readOnly = true)
   public Optional<Admin> findById(UUID id) {
-    return jAdminRepository.findById(id).map(jAdminMapper::toDomain);
+    return repository.findById(id).map(mapper::toDomain);
   }
 
   @Transactional
   public Admin save(Admin admin) {
-    return jAdminMapper.toDomain(jAdminRepository.save(jAdminMapper.toEntity(admin)));
+    return mapper.toDomain(repository.save(mapper.toEntity(admin)));
   }
 }
